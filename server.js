8c1039c0b9f497d83730f9d34ac3dff1ca2df31c
grodaaa <engstrom.kristinaa@gmail.com>
2018-08-13 22:52:17 +0200

    Simple websockets

--- server.js
+++ server.js
@@ -1,32 +1,38 @@
-// server.js
-
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080;        
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/code-test')
-  .then(() =>  console.log('MongoDB <3 Mongoose: connection succesfull'))
+  .then(() =>  console.log('MongoDB 💙 Mongoose: connection succesfull'))
   .catch((err) => console.error(err));
 
 const api = require('./server/routes/api');
 
 app.use('/api', api);
 
-// START THE SERVER
-// =============================================================================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.get('/', function(req, res) {
+    res.sendfile('./app/src/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('A new WebSocket connection has been established');
+});
+
+setInterval(function() {
+    var stockprice = Math.floor(Math.random() * 1000);
+    io.emit('stock price update', stockprice);
+  }, 50);
+  
+http.listen(port, function() {
+    console.log('Magic happens on port ' + port);
+});
+
